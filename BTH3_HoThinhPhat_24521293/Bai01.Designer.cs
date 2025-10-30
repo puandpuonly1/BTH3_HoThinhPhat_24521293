dc2f4cb4a084c600096c6c445bb0f7996750abdd
Thinh Phat Ho <hothinhphat06@gmail.com>
2025-10-30 22:09:42 +0700

    FINAL

--- BTH3_HoThinhPhat_24521293/Bai01.Designer.cs
+++ BTH3_HoThinhPhat_24521293/Bai01.Designer.cs
@@ -28,18 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Label = new System.Windows.Forms.Label();
+            this.MoButton = new System.Windows.Forms.Button();
+            this.DongButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label.Location = new System.Drawing.Point(147, 166);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(298, 359);
+            this.Label.TabIndex = 0;
+            // 
+            // MoButton
+            // 
+            this.MoButton.Location = new System.Drawing.Point(773, 166);
+            this.MoButton.Name = "MoButton";
+            this.MoButton.Size = new System.Drawing.Size(146, 73);
+            this.MoButton.TabIndex = 1;
+            this.MoButton.Text = "Mở";
+            this.MoButton.UseVisualStyleBackColor = true;
+            this.MoButton.Click += new System.EventHandler(this.MoButton_Click);
+            // 
+            // DongButton
+            // 
+            this.DongButton.Location = new System.Drawing.Point(773, 452);
+            this.DongButton.Name = "DongButton";
+            this.DongButton.Size = new System.Drawing.Size(146, 73);
+            this.DongButton.TabIndex = 1;
+            this.DongButton.Text = "Đóng";
+            this.DongButton.UseVisualStyleBackColor = true;
+            this.DongButton.Click += new System.EventHandler(this.DongButton_Click);
             // 
             // Bai01
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1358, 770);
+            this.Controls.Add(this.DongButton);
+            this.Controls.Add(this.MoButton);
+            this.Controls.Add(this.Label);
             this.Name = "Bai01";
+            this.Activated += new System.EventHandler(this.Bai01_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Bai01_FormClosing);
+            this.Load += new System.EventHandler(this.Bai01_Load);
+            this.Shown += new System.EventHandler(this.Bai01_Shown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Button MoButton;
+        private System.Windows.Forms.Button DongButton;
     }
 }
--- BTH3_HoThinhPhat_24521293/Bai01.cs
+++ BTH3_HoThinhPhat_24521293/Bai01.cs
@@ -16,5 +16,37 @@ namespace BTH3_HoThinhPhat_24521293
         {
             InitializeComponent();
         }
+
+        private void Bai01_Activated(object sender, EventArgs e)
+        {
+            Label.Text += "Bai01 activated\n";
+        }
+
+        private void Bai01_Load(object sender, EventArgs e)
+        {
+            Label.Text += "Bai01 load\n";
+        }
+
+        private void Bai01_Shown(object sender, EventArgs e)
+        {
+            Label.Text += "Bai01 shown\n";
+        }
+
+        private void MoButton_Click(object sender, EventArgs e)
+        {
+            Form f = new Form();
+            Label.Text += "Mở thêm form con\n";
+            f.Show();
+        }
+
+        private void DongButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Bai01_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Chắc chắn không?", "Tắt form", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
     }
 }
